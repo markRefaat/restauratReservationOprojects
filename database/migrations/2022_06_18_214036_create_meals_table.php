699e4b6616d89c615e69782e48cbebd706321e1a
Mark Refaat <mark1711712@miuegypt.edu.eg>
2022-06-19 02:30:53 +0200

    migration files

---
+++ database/migrations/2022_06_18_214036_create_meals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->double('price', 10, 2);
+            $table->longText('description');
+            $table->integer('quantity_available');
+            $table->double('discount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+}
